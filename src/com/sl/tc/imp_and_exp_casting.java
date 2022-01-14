2ed82a34859c3df64a3d31663d9517c6460b079b
Anand <anandaloore@gmail.com>
2022-01-14 16:18:57 +0530

    first commit

---
+++ src/com/sl/tc/imp_and_exp_casting.java
@@ -0,0 +1,32 @@
+package com.sl.tc;
+
+public class imp_and_exp_casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			
+	int i = 10;
+			
+			// implicit Typecasting
+			float f = i;
+			 
+			double d = 45.54;
+			
+			// explicit typeCasting
+			int  i2= (int)d;
+
+			System.out.println("int value : " +i);
+
+			System.out.println("float value : " +f);
+			
+			System.out.println("double value : " +d);
+
+			System.out.println("int value : " +i2);
+
+		}
+
+
+	
+
+}
